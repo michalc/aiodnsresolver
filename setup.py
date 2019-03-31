03c2f79fb9f1ee5d9334c70c860b51b295def7b3
Michal Charemza <michal@charemza.name>
2019-03-31 13:25:13 +0100

    (release) v0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='aiodnsresolver',
-    version='0.0.2',
+    version='0.0.3',
     description='aiodnsresolver',
     long_description=long_description(),
     long_description_content_type='text/markdown',
