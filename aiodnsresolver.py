aaae0b0065593ff50654b4103e33c8da00a7ae9b
Michal Charemza <michal@charemza.name>
2019-04-04 21:09:02 +0100

    (refactor) Consistency

--- aiodnsresolver.py
+++ aiodnsresolver.py
@@ -147,7 +147,8 @@ def parse(data):
     qid, x, qd_num, an_num, ns_num, ar_num = struct.unpack('!HHHHHH', data[:12])
     r, z, ra, rd, tc, aa, o, qr = split_bits(x, 4, 3, 1, 1, 1, 1, 4, 1)
 
-    l, qd = parse_group(parse_request_record, 12, qd_num)
+    l = 12
+    l, qd = parse_group(parse_request_record, l, qd_num)
     l, an = parse_group(parse_response_record, l, an_num)
     l, ns = parse_group(parse_response_record, l, ns_num)
     l, ar = parse_group(parse_response_record, l, ar_num)
