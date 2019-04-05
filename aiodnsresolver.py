61930e41c50d2b58d9cbf859fbf413f626591a28
Michal Charemza <michal@charemza.name>
2019-04-05 08:01:10 +0100

    (refactor) Bit more duplication, less indirection

--- aiodnsresolver.py
+++ aiodnsresolver.py
@@ -123,17 +123,14 @@ def parse(data):
 
         return ResponseRecord(name, qtype, qclass, ttl, rdata)
 
-    def parse_group(record_parser, n):
-        return tuple(record_parser() for _ in range(n))
-
     qid, x, qd_count, an_count, ns_count, ar_count = struct.unpack('!HHHHHH', data[:12])
     rcode, z, ra, rd, tc, aa, opcode, qr = split_bits(x, 4, 3, 1, 1, 1, 1, 4, 1)
 
     l = 12
-    qd = parse_group(parse_request_record, qd_count)
-    an = parse_group(parse_response_record, an_count)
-    ns = parse_group(parse_response_record, ns_count)
-    ar = parse_group(parse_response_record, ar_count)
+    qd = tuple(parse_request_record() for _ in range(qd_count))
+    an = tuple(parse_response_record() for _ in range(an_count))
+    ns = tuple(parse_response_record() for _ in range(ns_count))
+    ar = tuple(parse_response_record() for _ in range(ar_count))
 
     return DNSMessage(qid, qr, opcode, aa, tc, rd, ra, rcode, qd, an, ns, ar)
 
