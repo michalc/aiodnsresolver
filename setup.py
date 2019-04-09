6cd1cf4ea7875a45f23fd821cb295386bdd11c62
Michal Charemza <michal@charemza.name>
2019-04-09 07:28:10 +0100

    (build) Python 3.5 is not supported

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setuptools.setup(
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
