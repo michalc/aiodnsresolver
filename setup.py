98e665e147d14fa44279b2e1e81f524a7e7f00f6
Michal Charemza <michal@charemza.name>
2019-04-24 07:14:41 +0100

    (release) v0.0.109

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def tests_require():
 
 setuptools.setup(
     name='aiodnsresolver',
-    version='0.0.108',
+    version='0.0.109',
     description='Pure asyncio Python DNS resolver',
     long_description=long_description(),
     long_description_content_type='text/markdown',
