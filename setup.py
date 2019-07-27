4cc87558b2c20524daefc2af14cc876962c171a7
Michal Charemza <michal@charemza.name>
2019-07-27 14:27:04 +0100

    (release) v0.0.143

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='aiodnsresolver',
-    version='0.0.142',
+    version='0.0.143',
     description='Pure asyncio Python DNS resolver',
     long_description=long_description(),
     long_description_content_type='text/markdown',
