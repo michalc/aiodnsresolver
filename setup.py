b48254ad06cca763d2022e8cf495aeb6b78d9718
Michal Charemza <michal@charemza.name>
2023-08-27 07:38:52 +0100

    ci: test and lint on Python 3.8.0, and drop Python 3.6.4
    For some reason aiohttp results in server disconnected errors if running tests
    from setup.py on Python 3.8.0. This is deprecated anyway, so now running tests
    more directly
    
    And for some reason on Python 3.6.4 aiohttp didn't install multidict, so
    dropping testing on that for now at least - suspect it's not worth it

--- setup.py
+++ setup.py
@@ -27,16 +27,6 @@ setuptools.setup(
     keywords='async dns asyncio resolver gethostbyname getaddrinfo',
     python_requires='>=3.6.4',
     test_suite='test',
-    tests_require=[
-        'aiofastforward~=0.0.24',
-        'aiohttp~=3.5.4',
-        'async-timeout~=3.0.1',
-        'attrs~=19.1.0',
-        'chardet~=3.0.4',
-        'idna~=2.8',
-        'multidict~=4.5.2',
-        'yarl~=1.3.0',
-    ],
     py_modules=[
         'aiodnsresolver',
     ],
